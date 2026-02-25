8d4e9f87054a6ac3322297429626fa1d19dc1dfb
Viktor-Sokorenko <viktor.sokorenko@student.howest.be>
2026-02-25 18:18:44 +0100

    Product.js-Update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,17 @@
 // Product.js
 class Product {
   calculatePrice() {
-    return this.basePrice + this.getTax();
+    return this.basePrice - this.getDiscount();
   }
   
-  getTax() {
-    return this.basePrice * this.taxRate;
+  getDiscount() {
+    return this.basePrice * this.discountRate;
   }
 }
 
 // pricing.js
 export const calculateTotal = (items) => {
   return items.reduce((sum, item) => {
-    return sum + item.calculatePrice() * (1 + getTax());
+    return sum + item.calculatePrice();
   }, 0);
 };
